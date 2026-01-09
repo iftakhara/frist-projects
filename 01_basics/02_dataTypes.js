e7277385816e9870e420388c0b14c3291438e201
iftakhara <iftakharshawon5@gmail.com>
2026-01-09 19:55:41 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use stricct"
+
+let = state ; 
+
+
+// Number => 2 to power 53 
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined =>
+// symbol => unique 
+
+// objacet 
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null); // objcet 
+
