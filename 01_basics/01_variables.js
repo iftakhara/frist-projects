83321df7fd0a7076984c8d5e0460fe006519088b
iftakhara <iftakharshawon5@gmail.com>
2026-01-09 19:09:20 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 133544
+let accountEamile = "iftakharshawon4@gmail.com"
+var accountPassword = "44552"
+accountcity  = "cumilla"
+
+
+// accountId = 2 
+accountEamile = "iftakharshawon5@gmail.com"
+accountPassword = "55552"
+accountcity  = "Debaidwar"
+
+console.table([accountEamile,accountPassword, accountcity]);
+
+console.log(accountEamile);
